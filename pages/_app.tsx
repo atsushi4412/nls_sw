db94cd75967b82e96678e2a22e32e0ae08a418c1
atsushi4412 <atsushi.nextlinks@gmail.com>
2023-01-23 14:38:28 +0900

    2023-01-23_4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,10 +19,14 @@ import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import ReactLoading from "react-loading";
+import { useRouter } from "next/router";
+import internal from "stream";
 
 Amplify.configure(awsconfig);
 
 const queryClient = new QueryClient();
+
+const url = "https://clnjb72gh0.execute-api.ap-northeast-1.amazonaws.com/bata";
 
 const App = (props: AppProps) => {
   return (
@@ -34,20 +38,10 @@ const App = (props: AppProps) => {
   );
 };
 
-/*export async function getPower() {
-  const response = await fetch(
-    "https://clnjb72gh0.execute-api.ap-northeast-1.amazonaws.com/bata?power=1&emg=0&emg_rel=0"
-  );
-  const jsonData = await response.json();
-  return jsonData.items.map((elem: any) => {
-    return {
-      Power: elem.power,
-      Emg: elem.emg,
-      Emg_Release: elem.emg_rel,
-    };
-  });
+async function getData_power(p: string, e: string, e_r: string) {
+  const a = await fetch(url + p + e + e_r);
 }
-*/
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [sidebarClosed, setSidebarClosed] = useState(true);
   const onSidebarToggle = (closed: boolean) => setSidebarClosed(closed);
@@ -58,18 +52,54 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   const [power_btn_color, setBtnColor] = useState(false);
   const [emg_btn_color, setEMGColor] = useState(false);
 
+  const router = useRouter();
+
   const onmousedown = () => {
     setBtnColor(true);
-    // const data = getPower();
+    getData_power("1", "0", "0");
+    /*router.push({
+      pathname:url,       //URL
+      query: {power : 1,
+              emg : 0,
+              emg_rel : 0} //検索クエリ
+    },"http://localhost:3000/");
+    */
   };
   const onmousup = () => {
     setBtnColor(false);
+    getData_power("0", "0", "0");
+    /*router.push({
+      pathname:url,       //URL
+      query: {power : 0,
+              emg : 0,
+              emg_rel : 0} //検索クエリ
+    },"http://localhost:3000/");
+    */
   };
   const cls = power_btn_color
     ? "bg-green-500 font-semibold text-white py-8 px-8 rounded m-5"
     : "bg-stone-500 font-semibold text-white py-8 px-8 rounded m-5";
 
   const onmouseclick = () => {
+    if (!emg_btn_color) {
+      getData_power("0", "1", "0");
+      /*router.push({
+        pathname:url,       //URL
+        query: {power : 0,
+                emg : 1,
+                emg_rel : 0},
+      },"http://localhost:3000/");
+      */
+    } else {
+      getData_power("0", "0", "1");
+      /* router.push({
+        pathname:url,       //URL
+        query: {power : 0,
+                emg : 0,
+                emg_rel : 1} //検索クエリ
+      },"http://localhost:3000/");
+      */
+    }
     setEMGColor(!emg_btn_color);
   };
   const cls2 = emg_btn_color
@@ -96,7 +126,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           <main>
             <div className="h-screen w-screen flex justify-center items-center">
               <button
-                id="area1"
                 onMouseDown={onmousedown}
                 onMouseUp={onmousup}
                 className={cls}
